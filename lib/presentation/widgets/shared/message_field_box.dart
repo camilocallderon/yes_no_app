19737168dd9fe6d9a2e193b639810666018a7e36
Camilo Calderón Rocha <46984337+camilocallderon@users.noreply.github.com>
2024-02-03 20:11:47 -0500

    feat(message_field_box): add textformfield with style

--- lib/presentation/widgets/shared/message_field_box.dart
+++ lib/presentation/widgets/shared/message_field_box.dart
@@ -5,6 +5,33 @@ class MessageFieldBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final outlineInputBorder = UnderlineInputBorder(
+      borderSide: const BorderSide(color: Colors.transparent),
+      borderRadius: BorderRadius.circular(40),
+    );
+
+    final inpurDecoration = InputDecoration(
+      enabledBorder: outlineInputBorder,
+      focusedBorder: outlineInputBorder,
+      filled: true,
+      suffixIcon: IconButton(
+        icon: const Icon(
+          Icons.send_outlined,
+        ),
+        onPressed: () {
+          print('valor de la cja de texto?');
+        },
+      ),
+    );
+
+    return TextFormField(
+      decoration: inpurDecoration,
+      onFieldSubmitted: (value) {
+        print('Submit value: $value');
+      },
+      onChanged: (value) {
+        print('changed: $value');
+      },
+    );
   }
 }
