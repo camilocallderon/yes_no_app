4fe4ba77a0a298b18fc637b72eb93e9df006f395
Camilo Calderón Rocha <46984337+camilocallderon@users.noreply.github.com>
2024-02-03 20:44:52 -0500

    feat(message_field_box): add focusnode and textcontroller

--- lib/presentation/widgets/shared/message_field_box.dart
+++ lib/presentation/widgets/shared/message_field_box.dart
@@ -5,12 +5,16 @@ class MessageFieldBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final textController = TextEditingController();
+    final focusNode = FocusNode();
+
     final outlineInputBorder = UnderlineInputBorder(
       borderSide: const BorderSide(color: Colors.transparent),
       borderRadius: BorderRadius.circular(40),
     );
 
     final inpurDecoration = InputDecoration(
+      hintText: 'End your message with a "?"',
       enabledBorder: outlineInputBorder,
       focusedBorder: outlineInputBorder,
       filled: true,
@@ -19,18 +23,24 @@ class MessageFieldBox extends StatelessWidget {
           Icons.send_outlined,
         ),
         onPressed: () {
-          print('valor de la cja de texto?');
+          final textValue = textController.value.text;
+          print('Button: $textValue');
+          textController.clear();
         },
       ),
     );
 
     return TextFormField(
+      onTapOutside: (event) {
+        focusNode.unfocus();
+      },
+      focusNode: focusNode,
+      controller: textController,
       decoration: inpurDecoration,
       onFieldSubmitted: (value) {
         print('Submit value: $value');
-      },
-      onChanged: (value) {
-        print('changed: $value');
+        textController.clear();
+        focusNode.requestFocus();
       },
     );
   }
